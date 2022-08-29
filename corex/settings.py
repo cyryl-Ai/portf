72a1734c1695cf96e6e62381f573f7d97ce6e52f
emmanuel <dide6611@gmail.com>
2022-08-29 08:32:43 +0100

    added requirements

--- corex/settings.py
+++ corex/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5f@*564o0+a-2s_$cswr71#f-ed&jkjg=4_jwk-8cak2k#e4#_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['verdena.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -64,8 +67,12 @@ WSGI_APPLICATION = 'corex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "verdena",
+        "USER": "postgres",
+        "PASSWORD": "#fikay20omij",
+        "HOST": "verdena.herokuapp.com",
+        "PORT": "5432"
     }
 }
 
@@ -103,6 +110,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/medias/'
